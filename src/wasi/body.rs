82d9243f9cee513043bb140b77282847ff7e76d5
kmatasfp <33095685+kmatas@users.noreply.github.com>
2025-06-22 05:53:21 -0700

    fix: use blocking write

--- src/wasi/body.rs
+++ src/wasi/body.rs
@@ -193,7 +193,7 @@ impl Body {
     ) -> Result<(), crate::Error> {
         match self.kind.take().expect("Body has already been extracted") {
             Kind::Reader(mut reader, _) => {
-                let mut buf = [0; 2];
+                let mut buf = [0; 4 * 1024];
                 loop {
                     let len = reader.read(&mut buf).map_err(crate::error::builder)?;
                     if len == 0 {
@@ -203,7 +203,7 @@ impl Body {
                 }
                 Ok(())
             }
-            Kind::Bytes(bytes) => ChunkIter::new(&bytes, 2).try_for_each(&mut f),
+            Kind::Bytes(bytes) => ChunkIter::new(&bytes, 4 * 1024).try_for_each(&mut f),
             Kind::Incoming(ref body_stream) => {
                 let mut eof = false;
                 while !eof {
--- src/wasi/client.rs
+++ src/wasi/client.rs
@@ -216,8 +216,7 @@ impl Client {
                 .write()
                 .map_err(|e| failure_point("write", e))?;
             body.write(|chunk| {
-                request_body_stream.write(chunk)?;
-                request_body_stream.flush()?;
+                request_body_stream.blocking_write_and_flush(chunk)?;
                 Ok(())
             })?;
             drop(request_body_stream);
